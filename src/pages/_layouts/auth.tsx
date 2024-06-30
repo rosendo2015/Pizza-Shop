69cb9956a80acf1abfd837f047d8f7515f2c88f4
rosendo <rosendc30@gmail.com>
2024-06-30 16:31:13 -0300

    form sign-in

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom'
 export function AuthLayout() {
   return (
     <div className="grid min-h-screen grid-cols-2">
-      <div className="flex h-full flex-col justify-between border-r border-x-foreground/5 bg-muted p-10 text-muted-foreground">
+      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
         <div className="flex items-center gap-3 text-lg text-foreground">
           <Pizza className="h-5 w-5" />
           <span className="font-semibold">pizza.shop</span>
