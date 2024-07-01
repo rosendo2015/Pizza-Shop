f36ae2bdb03d905e3684806fcf7298eade26819d
rosendo <rosendc30@gmail.com>
2024-07-01 19:43:48 -0300

    sing-in e dashboard com helmet

--- src/pages/auth/sing-in.tsx
+++ src/pages/auth/sing-in.tsx
@@ -1,3 +1,5 @@
+import { Helmet } from 'react-helmet-async'
+
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -5,6 +7,7 @@ import { Label } from '@/components/ui/label'
 export function SingIn() {
   return (
     <>
+      <Helmet title="Login" />
       <div className="p-8">
         <div className="flex w-[358px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
