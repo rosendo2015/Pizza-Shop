8d8483d19db9dda6036de2a2983991a54a8bf189
rosendo <rosendc30@gmail.com>
2024-07-03 19:55:21 -0300

    toggle theme

---
+++ src/components/account-menu.tsx
@@ -0,0 +1,40 @@
+import { Building, ChevronDown } from 'lucide-react'
+
+import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+
+export function AccountMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          className="flex select-none items-center gap-2"
+        >
+          <ChevronDown className="h-4 w-4" />
+          Pizza Shop
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuLabel className="flex flex-col">
+          <span>Francisco Rosendo</span>
+          <span className="text-xs font-normal text-muted-foreground">
+            rosendc30@hotmail.com
+          </span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Building className="mr-2 h-4 w-4" />
+          <span>Perfil da loja</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
