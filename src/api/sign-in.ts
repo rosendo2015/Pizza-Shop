43eadca070030028b9bfdc3043fe69e6485df322
rosendo <rosendc30@gmail.com>
2024-07-07 17:47:13 -0300

    mutationFn

---
+++ src/api/sign-in.ts
@@ -0,0 +1,9 @@
+import { api } from '@/lib/axios'
+
+export interface SingInBody {
+  email: string
+}
+
+export async function signIn({ email }: SingInBody) {
+  await api.post('/authenticate', { email })
+}
