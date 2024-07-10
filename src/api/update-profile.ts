433a1160757b6b0f75a46b404775cef804d07427
rosendo <rosendc30@gmail.com>
2024-07-09 21:50:44 -0300

    update profile

---
+++ src/api/update-profile.ts
@@ -0,0 +1,8 @@
+import { api } from '@/lib/axios'
+interface UpdateProfileBody {
+  name: string
+  description: string | null
+}
+export async function updateProfile({ name, description }: UpdateProfileBody) {
+  await api.put('/profile', { name, description })
+}
