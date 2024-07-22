3d28ffdba78c9613f008d873e1b229251b9acc16
rosendo <rosendc30@gmail.com>
2024-07-22 19:26:45 -0300

    testing

--- src/components/ui/order-status.spec.tsx
+++ src/components/ui/order-status.spec.tsx
@@ -1,3 +1,15 @@
-test('1 + 1 equals 2', () => {
-  expect(1 + 1).toEqual(2)
+import { render } from '@testing-library/react'
+
+import { OrderStatus } from './order-status'
+
+describe('Order Status', () => {
+  it('should display the right when order status is pending', () => {
+    const wrapper = render(<OrderStatus status="pending" />)
+
+    const statusText = wrapper.getByText('Pendente')
+    const badgElement = wrapper.getByTestId('badge')
+
+    expect(statusText).toBeInTheDocument()
+    expect(badgElement).toHaveClass('bg-slate-400')
+  })
 })
