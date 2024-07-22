03faa4028a2ad57be62bc2d5e02b0c9b8163548c
rosendo <rosendc30@gmail.com>
2024-07-22 19:44:25 -0300

    teste

--- src/components/ui/order-status.spec.tsx
+++ src/components/ui/order-status.spec.tsx
@@ -12,4 +12,40 @@ describe('Order Status', () => {
     expect(statusText).toBeInTheDocument()
     expect(badgElement).toHaveClass('bg-slate-400')
   })
+  it('should display the right when order status is pending canceled', () => {
+    const wrapper = render(<OrderStatus status="canceled" />)
+
+    const statusText = wrapper.getByText('Cancelado')
+    const badgElement = wrapper.getByTestId('badge')
+
+    expect(statusText).toBeInTheDocument()
+    expect(badgElement).toHaveClass('bg-rose-500')
+  })
+  it('should display the right when order status is pending processing', () => {
+    const wrapper = render(<OrderStatus status="processing" />)
+
+    const statusText = wrapper.getByText('Preparando')
+    const badgElement = wrapper.getByTestId('badge')
+
+    expect(statusText).toBeInTheDocument()
+    expect(badgElement).toHaveClass('bg-amber-500')
+  })
+  it('should display the right when order status is pending delivering', () => {
+    const wrapper = render(<OrderStatus status="delivering" />)
+
+    const statusText = wrapper.getByText('Em entraga')
+    const badgElement = wrapper.getByTestId('badge')
+
+    expect(statusText).toBeInTheDocument()
+    expect(badgElement).toHaveClass('bg-sky-600')
+  })
+  it('should display the right when order status is pending delivered', () => {
+    const wrapper = render(<OrderStatus status="delivered" />)
+
+    const statusText = wrapper.getByText('Entrague')
+    const badgElement = wrapper.getByTestId('badge')
+
+    expect(statusText).toBeInTheDocument()
+    expect(badgElement).toHaveClass('bg-emerald-500')
+  })
 })
--- src/components/ui/order-status.tsx
+++ src/components/ui/order-status.tsx
@@ -18,19 +18,34 @@ export function OrderStatus({ status }: OrderStatusProps) {
   return (
     <div className="flex items-center gap-2">
       {status === 'pending' && (
-        <span className="h-2 w-2 rounded-full bg-slate-400"></span>
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-slate-400"
+        ></span>
       )}
       {status === 'canceled' && (
-        <span className="h-2 w-2 rounded-full bg-rose-500"></span>
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-rose-500"
+        ></span>
       )}
       {status === 'delivered' && (
-        <span className="h-2 w-2 rounded-full bg-emerald-500"></span>
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-amber-500"
+        ></span>
       )}
       {status === 'processing' && (
-        <span className="h-2 w-2 rounded-full bg-amber-500"></span>
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-amber-500"
+        ></span>
       )}
       {status === 'delivering' && (
-        <span className="h-2 w-2 rounded-full bg-sky-600"></span>
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-sky-600"
+        ></span>
       )}
 
       <span className="font-medium text-muted-foreground">
