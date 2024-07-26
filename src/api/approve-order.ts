087511fa17717e02282c507f739245cbb96efe4b
rosendo <rosendc30@gmail.com>
2024-07-25 23:46:40 -0300

    Orders

--- src/api/approve-order.ts
+++ src/api/approve-order.ts
@@ -3,6 +3,7 @@ import { api } from '@/lib/axios'
 export interface ApproveOrderParams {
   orderId: string
 }
+
 export async function approveOrder({ orderId }: ApproveOrderParams) {
   await api.patch(`/orders/${orderId}/approve`)
 }
--- src/api/cancel-order.ts
+++ src/api/cancel-order.ts
@@ -3,6 +3,7 @@ import { api } from '@/lib/axios'
 export interface CancelOrderParams {
   orderId: string
 }
+
 export async function cancelOrder({ orderId }: CancelOrderParams) {
   await api.patch(`/orders/${orderId}/cancel`)
 }
