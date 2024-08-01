1ebd30628c6d436558246f704f9b8dc95976d311
rosendo <rosendc30@gmail.com>
2024-07-31 22:27:47 -0300

    index de um mock

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -1,5 +1,7 @@
 import { setupWorker } from 'msw/browser'
+
 import { env } from '@/env'
+
 import { signInMock } from './sign-in-mock'
 export const worker = setupWorker(signInMock)
 export async function enableMSW() {
